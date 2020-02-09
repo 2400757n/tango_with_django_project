67ddd1189946d0327056cdec5754d3d711cdcaa5
Thomas Nordentoft <2400757n@student.gla.ac.uk>
2020-02-09 14:56:06 +0000

    added url template tag, added added template inheritance from base template, created custom template tags

--- rango/views.py
+++ rango/views.py
@@ -99,6 +99,7 @@ def add_page(request, category_name_slug):
     form = PageForm()
     
     if request.method == 'POST': 
+
         form = PageForm(request.POST)
 
         if form.is_valid(): 
@@ -107,11 +108,11 @@ def add_page(request, category_name_slug):
                 page.category = category 
                 page.views = 0 
                 page.save()
-                
                 return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
         
         else: 
             print(form.errors)
+
     context_dict = {'form': form, 'category': category} 
     return render(request, 'rango/add_page.html', context=context_dict)
 
